c88c444b8d4efb284c34b98b1a2aec7c3f7e8105
Jeremy <34408108+Je12emy@users.noreply.github.com>
2019-03-23 15:30:36 -0600

    Update added row printing

--- 5.uso de condicionales y loops.asm
+++ 5.uso de condicionales y loops.asm
@@ -1,10 +1,16 @@
+;*********** METODO PARA IMPRIMIR UNA LETRA N CANTIDAD DE VECES EN N FILAS ***********
+ 
+
 .model small
 .stack 100h
 .data
     mensaje1 db "Preciona una tecla:$"
     pregunta db "Cuantas veces desea imprimir esa tecla?:$"
+    pregunta2 db "En cuantas filas desea imprimir esa tecla?:$"
     tecla db "$"
     numero db "$"
+    num_filas db "$"
+    auxiliar db "$"
     espacio db 0AH,0DH,'$'
     
 .code
@@ -24,28 +30,43 @@
     call readline()
     
     mov numero,AL
+    mov auxiliar, AL
+    
     sub numero,30H           ;Quito el ascii
+    sub auxiliar, 30H
     call clear()
     
-    call imprimir_n_cantidad()
+    lea dx,pregunta2
+    call println()
+    call readline()
     
+    mov num_filas,AL
+    
+    
+    sub num_filas,30h        ;Remover el ascii
+    call clear()
+       
+    call imprimir_filas()
+
     halt:
     mov ah,4ch
     int 21h
     
+   
+   
 ;******** METODOS ********
 
 println() proc              ;Metodo generico para imprimir      
     mov ah,09h
     int 21h
     ret
-endp
+endp  
 
 printtecla() proc           ;Metodo para imprimir la tecla. 
     mov ah,02h              ;El servicio 02h imprime el contenido en DL, eso lo hace mas optimo para este caso.
     int 21h
     ret
-endp
+    endp
 
 readline() proc             ;NOTA: Este metodo guarda la entrada por teclado en AL.
     mov ah,01h
@@ -66,10 +87,25 @@ clear() proc                ;Metodo generico para limpiar la pantalla
     int 10h
     ret
 endp
+       
+imprimir_filas() proc           ;Metodo para imprimir n cantidad de filas 
+     
+    call imprimir_n_cantidad()  ;Imprimir n cantidad de letras
+    call enter()
+    
+    mov al,auxiliar
+    mov numero, al 
+        
+    dec num_filas
+    JNZ imprimir_filas()
+    ret  
+endp 
 
-imprimir_n_cantidad() proc  ;Metodo para imprimir la tecla n cantidad de veces.
-    mov  dl,tecla           ;Mueve tecla a DL para prepararla para printtecla().
+imprimir_n_cantidad() proc ;Metodo para imprimir la tecla n cantidad de veces.
+    
+    mov  dl,tecla          ;Mueve tecla a DL para prepararla para printtecla().
     call printtecla()
+    
     dec numero
     JNZ imprimir_n_cantidad()
     ret        
